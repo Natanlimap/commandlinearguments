3e0a7464d94a593cdcf8cc81d99d681f49bc348c
Natanlimap <natanlimap@gmail.com>
2020-06-17 10:26:27 -0300

    :rocket: output feito em um arquivo de texto

--- main.cpp
+++ main.cpp
@@ -11,23 +11,42 @@ int main(int argc, char const *argv[]) //funcao que mostra a implementacao de co
 		std::cout << "Uso: ./prog add <mensagem>" << std::endl; //Mostra para o usuario a entrada correta para o programa.
 		return 1;
 	}
+
+	//verifica se a outro argumento na linha de comando alem do nome do programa, e se esse comando se chamada "add"
 	else if(argc >= 2 && !(strcmp(argv[1], "add")))
-	{ //verifica se a outro argumento na linha de comando alem do nome do programa, e se esse comando se chamada "add"
+	{ 
+		//mensagem enviada pelo cliente
 		std::string message;
-		std::ofstream file; //Arquivo que ira receber 
+
+		//Arquivo que ira receber o output
+		std::ofstream file;  
+		
+		//se o programa receber 2 argumentos, significa que o cliente nao passou a mensagem
 		if(argc == 2)
-		{ //se o programa receber 2 argumentos, significa que o cliente nao passou a mensagem
-			getline(std::cin, message); //recebemos o input da mensagem do cliente
-			std::cout << "Mensagem Adicionada" << std::endl; //mostramos que a mensagem foi adicionada
+		{ 
 
+			std::cout << "Digite a mensagem" << std::endl;
+
+			//recebemos o input da mensagem do cliente
+			getline(std::cin, message); 
+			std::cout << "Mensagem Adicionada" << std::endl;
 		}else{
-			message = argv[2]; //como a mensagem foi passada pelo cliente, receberemos ela aqui.
-		}
-		file.open("texto.txt", std::ios::app);
-		file << message << std::endl;
-		file.close();
 
-		std::cout << message << std::endl; //imprimimos a mensagem
+			//neste caso, a mensagem  foi passada pelo cliente via terminal, receberemos ela aqui.
+			message = argv[2]; 
+		}
+
+		//abrindo o arquivo chamado texto.txt
+		file.open("texto.txt", std::ios::app); 
+
+		//Escrevemos a mensagem
+		file << message << std::endl;
+
+		//Fechamos o arquivo
+		file.close();
+		
+		//imprimimos a mensagem
+		std::cout << message << std::endl;
 	}
 	return 0; //programa executado corretamente
 }
